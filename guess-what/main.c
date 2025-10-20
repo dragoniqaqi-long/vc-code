ebb9daae845ccc7f0e558a19607b578f55e3d130
dragoniqaqi-long <dragoniqaqi@gmail.com>
2025-10-20 23:45:54 +0800

    已改答案

--- guess-what/main.c
+++ guess-what/main.c
@@ -2,24 +2,30 @@
 # include <stdlib.h>
 # include <time.h>
 
-
 int main(void)
 {
 	int guessnum;
 	srand ( (unsigned) time (NULL) );    //设置随机数(这里同时运用了两个函数,不用理解)
 	int ans = rand() % 100 ;    //此处设置随机数范围在1-99  这两行可以网络上了解,暂时不用理解   
-	int c = 0; //以c为错误次数,没错一次,c++一次 (以上两行代码可以理解为设置a成为了1-99的随机数,且每次运行程序都是独立不同的数,那么a就是我们需要猜的数)
+	//以上两行代码可以理解为设置a成为了1-99的随机数,且每次运行程序都是独立不同的数,那么a就是我们需要猜的数
+	
+	int c = 0;int d; //以c为错误次数,每错一次,c++一次 
 	printf("guess game\n"
 	     "请输入一个小于100的数字\n");
 	scanf("%d", &guessnum);
+	d = 7;
 	
-	int d = 7;          //猜100以内数,设置机会为7次
-	                             //使用while循环,条件即为没猜到a并且错误次数小于试错次数的情况下执行
-	while (ans != guessnum&&c < d)       
+	//猜100以内数,假设试错次数为7次
+			//使用while循环,条件即为没猜到a并且错误次数小于试错次数的情况下执行
+	while (ans != guessnum && c < d)       
 	{
 		c++;
-
-        if (ans < guessnum)                  
+		if (d == c && ans != guessnum)
+		{
+			printf("次数用尽,游戏结束,正确答案是%d", ans);
+			return 0;
+		}
+		if (ans < guessnum)                  
 		{
 			printf("大了,请重新输入\n");
 			printf("你现在拥有%d次机会\n", d - c);
@@ -43,15 +49,18 @@ int main(void)
 			printf("你现在拥有%d次机会\n", d - c);     →这个也是错的
 			scanf_s("%d", &b);
 		}*/
+		
+		
 	}
 	if (ans == guessnum)
 	{
 		printf("正确,你猜对了正确答案%d\n", ans);
+		return 0;
 	}
 
-	if (d == c && ans != guessnum)
+	/*if (d == c && ans != guessnum)
 	{
 		printf("次数用尽,游戏结束,正确答案是%d", ans);
-	}
+	}*/
 	return 0;
 }
--- jump-and-down/main.c
+++ jump-and-down/main.c
@@ -4,3 +4,23 @@
 当第10次落地后,小球的路程是?小球反弹的高度又是?
 
 */
+#include <stdio.h>
+
+double down (double a)
+{
+    return a * 0.5;
+}
+
+int main()
+{   
+    int a = 100;
+    for(int i = 1;i <= 10;i++)
+    {
+        down(a);
+    }
+
+
+
+
+    return 0;
+}
